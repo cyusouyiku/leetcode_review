a55009470b442a04cd5b56c59d13d441bbb5dd1a
Andy Zhang <17824805921@163.com>
2026-05-01 19:09:51 +0800

    Add minRemoval function to balance array
    Implement a solution to find the minimum number of removals needed to balance an array based on a given condition.

---
+++ 3634.cpp
@@ -0,0 +1,19 @@
+//3634. 使数组平衡的最少移除数目
+
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        int ans = n, right = 0;
+        for (int left = 0; left < n; ++left) {
+            while (right < n && nums[right] <= static_cast<long long>(nums[left]) * k) {
+                ++right;
+            }
+            ans = min(ans, n - (right - left));
+        }
+
+        return ans;
+    }
+};
